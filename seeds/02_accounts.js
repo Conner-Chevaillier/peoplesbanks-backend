976d771137aa47e538385137731395e4c3959360
grantfogle <grantjfogle@gmail.com>
2018-12-13 14:47:37 -0700

    updating queries and have all routes except create and delete

--- seeds/02_accounts.js
+++ seeds/02_accounts.js
@@ -5,13 +5,13 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('accounts').insert([
-        { bank_name: 'Bank of the People', acct_type: 'Checking', user_id: 1 },
-        { bank_name: 'Bank of the People', acct_type: 'Checking', user_id: 2 },
-        { bank_name: 'Bank of the People', acct_type: 'Saving', user_id: 2 },
-        { bank_name: 'Bank of the People', acct_type: 'Saving', user_id: 3 },
-        { bank_name: 'Bank of the People', acct_type: 'Checking', user_id: 4 },
-        { bank_name: 'Bank of the People', acct_type: 'Saving', user_id: 4 },
-        { bank_name: 'Bank of the People', acct_type: 'Checking', user_id: 5 }
+        { acct_name: 'My Checking', bank_name: 'Bank of the People', acct_type: 'Checking', total: 10475, user_id: 1 },
+        { acct_name: 'Hookers and Blow', bank_name: 'Bank of the People', acct_type: 'Checking', total: 19475, user_id: 2 },
+        { acct_name: 'House', bank_name: 'Bank of the People', acct_type: 'Saving', total: 20475, user_id: 2 },
+        { acct_name: 'Crack and Stuff', bank_name: 'Bank of the People', acct_type: 'Saving', total: 800000, user_id: 3 },
+        { acct_name: 'Good Stuff', bank_name: 'Bank of the People', acct_type: 'Checking', total: 70000, user_id: 4 },
+        { acct_name: 'Just Blow', bank_name: 'Bank of the People', acct_type: 'Saving', total: 7500, user_id: 4 },
+        { acct_name: 'Just Hookers', bank_name: 'Bank of the People', acct_type: 'Checking', total: 75000, user_id: 5 }
       ]);
     });
 };
