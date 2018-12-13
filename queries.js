1e4f763ccb3955c1ea175d0477e3ed5ffb831d6e
grantfogle <grantjfogle@gmail.com>
2018-12-13 12:09:27 -0700

    set up all crud user routes

--- queries.js
+++ queries.js
@@ -3,5 +3,17 @@ const db = require('./database-connection');
 module.exports = {
     listAllUsers() {
         return db('users');
+    },
+    getUserById(id) {
+        return db('users').where('id', id);
+    },
+    createUser(newUser) {
+        return db('users').insert(newUser).returning('*');
+    },
+    updateUser(id, user) {
+        return db('users').where('id', id).update(user).returning('*');
+    },
+    deleteUser(id) {
+        return db('users').where('id', id).delete();
     }
 }
